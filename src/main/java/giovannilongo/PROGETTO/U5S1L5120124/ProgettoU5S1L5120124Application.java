018bf1551f58a10790360d61b05537e2d2e935d2
Giovanni Longo <giovanni.longo10@icloud.com>
2024-01-12 11:15:54 +0100

    create springboot project and set ap

---
+++ src/main/java/giovannilongo/PROGETTO/U5S1L5120124/ProgettoU5S1L5120124Application.java
@@ -0,0 +1,13 @@
+package giovannilongo.PROGETTO.U5S1L5120124;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoU5S1L5120124Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoU5S1L5120124Application.class, args);
+	}
+
+}
---
+++ src/test/java/giovannilongo/PROGETTO/U5S1L5120124/ProgettoU5S1L5120124ApplicationTests.java
@@ -0,0 +1,13 @@
+package giovannilongo.PROGETTO.U5S1L5120124;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoU5S1L5120124ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
